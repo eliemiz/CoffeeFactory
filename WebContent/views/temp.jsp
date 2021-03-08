6efd899be8a8aa16fa95d7db8d8d79f2ef92a874
ELIE <genofreedomi@naver.com>
2021-03-08 20:41:16 +0900

    Temp파일 추가

---
+++ WebContent/views/temp.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" import="java.util.*" import="java.net.*" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<c:set var="path" value="${pageContext.request.contextPath}"/> 
+<fmt:requestEncoding value="UTF-8" />
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript">
+	
+	$(document).ready(function(){
+		
+	});
+		
+</script>
+</head>
+<body>
+	<h3>temp view</h3>
+	<form method="post">
+		<input type="text" name="temp" value="${param.temp}">
+		<input type="submit" value="submit">
+	</form>
+	<div>${result}</div>
+	
+</body>
+</html>
---
+++ src/coffeeFactory/controller/TempController.java
@@ -0,0 +1,49 @@
+package coffeeFactory.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TempController
+ */
+@WebServlet(name = "temp.do", urlPatterns = { "/temp.do" })
+public class TempController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TempController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		// 요청
+		String temp = request.getParameter("temp");
+		if (temp == null) {
+			temp = "";
+		}
+		
+		// 모델
+		request.setAttribute("result", "결과값 : " + temp);
+		
+		// 뷰
+		String page = "views\\temp.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
+		
+	}
+
+}
