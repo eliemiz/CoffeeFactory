31cc0f1fec3d423d5285251f9915811df376a093
Isukim <aftersoul1994@gmail.com>
2021-03-14 16:21:19 +0900

    wish controller 추가

---
+++ src/coffeeFactory/controller/wishlist/WishlistController.java
@@ -0,0 +1,36 @@
+package coffeeFactory.controller.wishlist;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WishlistController
+ */
+@WebServlet(name = "wish.do", urlPatterns = { "/wish.do" })
+public class WishlistController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WishlistController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String page = "views\\wishlist\\wishlist.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
+	}
+}
