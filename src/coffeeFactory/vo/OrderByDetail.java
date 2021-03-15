2a893825efd19649e83ed0c2f6357d7826d1b518
Isukim <aftersoul1994@gmail.com>
2021-03-16 01:32:37 +0900

    cart,order,wish vo 수정

---
+++ src/coffeeFactory/vo/OrderByDetail.java
@@ -0,0 +1,185 @@
+package coffeeFactory.vo;
+
+import java.util.Date;
+
+public class OrderByDetail {
+	private int order_id; 
+	private int account_id; 
+	private int price; 
+	private String send_name; 
+	private String send_email; 
+	private String send_phone; 
+	private String send_phone2; 
+	private String send_address; 
+	private String recv_name; 
+	private String recv_phone; 
+	private String recv_phone2; 
+	private String recv_address; 
+	private Date order_date; 
+	private String order_date_s;
+	
+	public OrderByDetail() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderByDetail(int order_id, int account_id, int price, String send_name, String send_email,
+			String send_phone, String send_phone2, String send_address, String recv_name, String recv_phone,
+			String recv_phone2, String recv_address, Date order_date) {
+		super();
+		this.order_id = order_id;
+		this.account_id = account_id;
+		this.price = price;
+		this.send_name = send_name;
+		this.send_email = send_email;
+		this.send_phone = send_phone;
+		this.send_phone2 = send_phone2;
+		this.send_address = send_address;
+		this.recv_name = recv_name;
+		this.recv_phone = recv_phone;
+		this.recv_phone2 = recv_phone2;
+		this.recv_address = recv_address;
+		this.order_date = order_date;
+	}
+
+	public OrderByDetail(int order_id, int account_id, int price, String send_name, String send_email,
+			String send_phone, String send_phone2, String send_address, String recv_name, String recv_phone,
+			String recv_phone2, String recv_address, String order_date_s) {
+		super();
+		this.order_id = order_id;
+		this.account_id = account_id;
+		this.price = price;
+		this.send_name = send_name;
+		this.send_email = send_email;
+		this.send_phone = send_phone;
+		this.send_phone2 = send_phone2;
+		this.send_address = send_address;
+		this.recv_name = recv_name;
+		this.recv_phone = recv_phone;
+		this.recv_phone2 = recv_phone2;
+		this.recv_address = recv_address;
+		this.order_date_s = order_date_s;
+	}
+
+	public int getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+
+	public int getAccount_id() {
+		return account_id;
+	}
+
+	public void setAccount_id(int account_id) {
+		this.account_id = account_id;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getSend_name() {
+		return send_name;
+	}
+
+	public void setSend_name(String send_name) {
+		this.send_name = send_name;
+	}
+
+	public String getSend_email() {
+		return send_email;
+	}
+
+	public void setSend_email(String send_email) {
+		this.send_email = send_email;
+	}
+
+	public String getSend_phone() {
+		return send_phone;
+	}
+
+	public void setSend_phone(String send_phone) {
+		this.send_phone = send_phone;
+	}
+
+	public String getSend_phone2() {
+		return send_phone2;
+	}
+
+	public void setSend_phone2(String send_phone2) {
+		this.send_phone2 = send_phone2;
+	}
+
+	public String getSend_address() {
+		return send_address;
+	}
+
+	public void setSend_address(String send_address) {
+		this.send_address = send_address;
+	}
+
+	public String getRecv_name() {
+		return recv_name;
+	}
+
+	public void setRecv_name(String recv_name) {
+		this.recv_name = recv_name;
+	}
+
+	public String getRecv_phone() {
+		return recv_phone;
+	}
+
+	public void setRecv_phone(String recv_phone) {
+		this.recv_phone = recv_phone;
+	}
+
+	public String getRecv_phone2() {
+		return recv_phone2;
+	}
+
+	public void setRecv_phone2(String recv_phone2) {
+		this.recv_phone2 = recv_phone2;
+	}
+
+	public String getRecv_address() {
+		return recv_address;
+	}
+
+	public void setRecv_address(String recv_address) {
+		this.recv_address = recv_address;
+	}
+
+	public Date getOrder_date() {
+		return order_date;
+	}
+
+	public void setOrder_date(Date order_date) {
+		this.order_date = order_date;
+	}
+
+	public String getOrder_date_s() {
+		return order_date_s;
+	}
+
+	public void setOrder_date_s(String order_date_s) {
+		this.order_date_s = order_date_s;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderByDetail [order_id=" + order_id + ", account_id=" + account_id + ", price=" + price
+				+ ", send_name=" + send_name + ", send_email=" + send_email + ", send_phone=" + send_phone
+				+ ", send_phone2=" + send_phone2 + ", send_address=" + send_address + ", recv_name=" + recv_name
+				+ ", recv_phone=" + recv_phone + ", recv_phone2=" + recv_phone2 + ", recv_address=" + recv_address
+				+ ", order_date=" + order_date + ", order_date_s=" + order_date_s + "]";
+	}
+	
+}
---
+++ src/coffeeFactory/vo/OrderByProduct.java
@@ -0,0 +1,58 @@
+package coffeeFactory.vo;
+
+public class OrderByProduct {
+	private int order_id;
+	private int product_id;
+	private String capacity;
+	private int grind_id;
+	private int count;
+	
+	public OrderByProduct() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public OrderByProduct(int order_id, int product_id, String capacity, int grind_id, int count) {
+		super();
+		this.order_id = order_id;
+		this.product_id = product_id;
+		this.capacity = capacity;
+		this.grind_id = grind_id;
+		this.count = count;
+	}
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(String capacity) {
+		this.capacity = capacity;
+	}
+	public int getGrind_id() {
+		return grind_id;
+	}
+	public void setGrind_id(int grind_id) {
+		this.grind_id = grind_id;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	@Override
+	public String toString() {
+		return "OrderByProduct [order_id=" + order_id + ", product_id=" + product_id + ", capacity=" + capacity
+				+ ", grind_id=" + grind_id + ", count=" + count + "]";
+	}
+	
+}
