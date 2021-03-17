b05da5ffec1bb79ae32b70c5abb9925ec6e18cdc
chaeyoung_lee <www970630@gmail.com>
2021-03-17 22:22:48 +0900

    review

--- src/coffeeFactory/controller/review/ReviewReplyController.java
+++ src/coffeeFactory/controller/review/ReviewReplyController.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import coffeeFactory.dao.DaoReview;
+import coffeeFactory.vo.Review;
+
 /**
  * Servlet implementation class ReviewReplyController
  */
@@ -30,13 +33,43 @@ public class ReviewReplyController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		
-		// 요청
-		
-		
-		
-		// 모델
-				
+		String proc= request.getParameter("proc");
+		 
+	      String review_idS = request.getParameter("review_id");
+	      //숫자형 데이터에 대한 처리(에러 및 예외 처리)
+	      if(review_idS==null) review_idS="0";
+	      int review_id=0;
+	      //숫자형 입력하지 않더라도 에러로 수행을 정지시키는것을 막을 수 있다.
+	      //문자열형으로 입력하면 review_id를 0으로 처리
+	      try {
+	    	  review_id = Integer.parseInt(review_idS);
+	      }catch(Exception e) {
+	    	  System.out.println(e.getMessage());
+	      }
+	      // 모델 
+	      DaoReview dao = new DaoReview();
+	      
+	      if(proc!=null) {
+	   
+	    	  if(proc.equals("upt")) {
+		    	  String product_id = request.getParameter("product_id");
+		    	  String account_id = request.getParameter("account_id");
+		    	  String regist_date_s = request.getParameter("regist_date_s");
+		    	  String rating = request.getParameter("rating");
+		    	  String title = request.getParameter("title");
+		    	  String content = request.getParameter("content");
+		    	  String image = request.getParameter("image");
+		    	  String reply_content = request.getParameter("reply_content");
+		    	  Review upt = new Review(review_id, Integer.parseInt(product_id),
+		    			  Integer.parseInt(account_id), regist_date_s,Integer.parseInt(rating),
+		    			  title,content,image,reply_content );
+		                   
+		    	  	dao.updateReview(upt);
+	    	  }
+	    	
+	    	  
+	      }
+	      request.setAttribute("rev", dao.getReview(review_id));
 		
 		
 		// 뷰
