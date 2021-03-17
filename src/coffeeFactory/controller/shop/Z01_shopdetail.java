de3bf98550ae81fcae56873d125bc9c6420c61c6
HR923 <zefyr923@gmail.com>
2021-03-17 19:56:38 +0900

    shop

--- src/coffeeFactory/controller/shop/Z01_shopdetail.java
+++ src/coffeeFactory/controller/shop/Z01_shopdetail.java
@@ -1,11 +1,15 @@
 package coffeeFactory.controller.shop;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import coffeeFactory.dao.DaoProduct;
 
 /**
  * Servlet implementation class Z01_shopdetail
@@ -27,6 +31,14 @@ public class Z01_shopdetail extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String product_id = request.getParameter("product_id");
+
+		DaoProduct dao = new DaoProduct();
+		
+	    String page = "views\\shop\\shop_front_detail.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
 	}
 
 }
