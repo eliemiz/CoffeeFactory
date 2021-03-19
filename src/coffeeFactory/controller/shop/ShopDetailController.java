08a09adb9e0908c0644f5cd7ae18b7b9113cc351
kwonmyoungji <ji39098578@gmail.com>
2021-03-19 14:07:06 +0900

    shopdetailcontroller qna

--- src/coffeeFactory/controller/shop/ShopDetailController.java
+++ src/coffeeFactory/controller/shop/ShopDetailController.java
@@ -53,6 +53,17 @@ public class ShopDetailController extends HttpServlet {
 
       
       // Q&A
+      /*
+      String proc = request.getParameter("proc");
+      String qna_idS = request.getParameter("qna_id");
+      if(qna_idS==null) qna_idS="0";
+      int qna_id=0;
+      try {
+    	  qna_id = Integer.parseInt(qna_idS);
+      } catch(Exception e) {
+    	  System.out.println(e.getMessage());
+      }
+      */
       
       // review
       
@@ -86,8 +97,17 @@ public class ShopDetailController extends HttpServlet {
       
       ArrayList<Qna> qlist = daoq.getQnaList(product_id);
       request.setAttribute("ac", daoa.getAccount(account_id));
+     /*
+      ArrayList<Qna> qlist2 = daoq.getQna(qna_id);
       
-      
+      if(proc!=null) {
+    	  if(proc.equals("del")) {
+        	  System.out.println("삭제준비완료: "+qna_id);
+        	  dao.deleteQna(qna_id);
+          }
+      }
+      request.setAttribute("Qna", qlist2);
+      */
       request.setAttribute("Qna", qlist);
       
       // review
