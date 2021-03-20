02957d897a4f309d7fd6093738cbd22a167864d2
Isukim <aftersoul1994@gmail.com>
2021-03-20 14:56:45 +0900

    pay 수정

--- src/coffeeFactory/controller/cart/PayController.java
+++ src/coffeeFactory/controller/cart/PayController.java
@@ -1,6 +1,7 @@
 package coffeeFactory.controller.cart;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import coffeeFactory.dao.DaoAccount;
+import coffeeFactory.dao.DaoCart;
+import coffeeFactory.dao.DaoOrderByDetail;
+import coffeeFactory.dao.DaoOrderByProduct;
+import coffeeFactory.dao.DaoProduct;
+import coffeeFactory.vo.Account;
+import coffeeFactory.vo.Cart;
 
 /**
  * Servlet implementation class PayController
@@ -39,18 +48,31 @@ public class PayController extends HttpServlet {
 		String send_phone = request.getParameter("send_phone"); 
 		String send_phone2 = request.getParameter("send_phone2"); 
 		String send_address = request.getParameter("send_address");
-
 		String comment = request.getParameter("comment");
 		String pay = request.getParameter("pay");
 	
-		
-		
+
 		int account_id = 0;
 		
 		HttpSession session = request.getSession();
 		Object account_id_obj = session.getAttribute("account_id");
 		if (account_id_obj != null) {
 			account_id = (int)account_id_obj;
+		}
+		DaoCart daoCart = new DaoCart();
+		DaoProduct daoProduct = new DaoProduct();
+		DaoOrderByDetail daoOrderDetail = new DaoOrderByDetail();
+		DaoOrderByProduct daoOrderProduct = new DaoOrderByProduct();
+		DaoAccount daoAccount = new DaoAccount();
+		
+		Account account = daoAccount.getAccount(account_id);
+		ArrayList<Cart> cartList = daoCart.getCartList(account_id);
+		
+		String proc = request.getParameter("proc");
+		if (proc != null) {	
+			if (proc.equals("order")) {
+				response.sendRedirect("views\\cart\\orderfin.jsp");
+			}
 		}
 		
 		String page = "views\\cart\\pay.jsp";
