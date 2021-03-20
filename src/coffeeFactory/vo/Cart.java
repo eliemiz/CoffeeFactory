fa2f58b2f0c2314ee216bc56fb59d4f1cd2523e3
Isukim <aftersoul1994@gmail.com>
2021-03-20 16:30:35 +0900

    cartVO 수정

--- src/coffeeFactory/vo/Cart.java
+++ src/coffeeFactory/vo/Cart.java
@@ -6,65 +6,62 @@ public class Cart {
 	private String capacity;
 	private int grind_id;
 	private int count;
-	
+	private int price;
 	public Cart() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	@Override
-	public String toString() {
-		return "Cart [account_id=" + account_id + ", product_id=" + product_id + ", capacity=" + capacity
-				+ ", grind_id=" + grind_id + ", count=" + count + "]";
-	}
-
-	public Cart(int account_id, int product_id, String capacity, int grind_id, int count) {
+	public Cart(int account_id, int product_id, String capacity, int grind_id, int count, int price) {
 		super();
 		this.account_id = account_id;
 		this.product_id = product_id;
 		this.capacity = capacity;
 		this.grind_id = grind_id;
 		this.count = count;
+		this.price = price;
 	}
-
 	public int getAccount_id() {
 		return account_id;
 	}
-
 	public void setAccount_id(int account_id) {
 		this.account_id = account_id;
 	}
-
 	public int getProduct_id() {
 		return product_id;
 	}
-
 	public void setProduct_id(int product_id) {
 		this.product_id = product_id;
 	}
-
 	public String getCapacity() {
 		return capacity;
 	}
-
 	public void setCapacity(String capacity) {
 		this.capacity = capacity;
 	}
-
 	public int getGrind_id() {
 		return grind_id;
 	}
-
 	public void setGrind_id(int grind_id) {
 		this.grind_id = grind_id;
 	}
-
 	public int getCount() {
 		return count;
 	}
-
 	public void setCount(int count) {
 		this.count = count;
 	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Cart [account_id=" + account_id + ", product_id=" + product_id + ", capacity=" + capacity
+				+ ", grind_id=" + grind_id + ", count=" + count + ", price=" + price + "]";
+	}
+	
+
 	
 }
