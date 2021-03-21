7112b25790ad0ca4736249fd9f0fe343e5f2c077
Isukim <aftersoul1994@gmail.com>
2021-03-21 14:53:21 +0900

    wish 수정

--- src/coffeeFactory/controller/wishlist/WishlistController.java
+++ src/coffeeFactory/controller/wishlist/WishlistController.java
@@ -8,6 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import coffeeFactory.dao.DaoCart;
+import coffeeFactory.dao.DaoWish;
+import coffeeFactory.vo.Wish;
 
 /**
  * Servlet implementation class WishlistController
@@ -29,6 +34,33 @@ public class WishlistController extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		String product_idS = request.getParameter("product_id");
+        if(product_idS==null) product_idS="0";
+        int product_id = Integer.parseInt(product_idS);
+		
+		
+		int account_id = 0;
+        HttpSession session = request.getSession();
+        Object account_id_obj = session.getAttribute("account_id");
+        if (account_id_obj != null) {
+           account_id = (int)account_id_obj;
+        }
+        
+        DaoWish daoWish = new DaoWish();
+        
+        String proc = request.getParameter("proc");
+        if (proc != null) {
+            if (proc.equals("addW")) {       
+               Wish wish = new Wish(0, 0);
+               daoWish.insertWish(wish);
+             } else if (proc.equals("delete")) {
+         	  DaoWish daoWishdel = new DaoWish(); 
+         	  daoWishdel.deleteWish(account_id, product_id);
+            }
+         } 
+		
+		
 		String page = "views\\wishlist\\wishlist.jsp";
 		RequestDispatcher rd = request.getRequestDispatcher(page);
 		rd.forward(request, response);
